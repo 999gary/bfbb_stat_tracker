c2708cc5f30076448df97d08d1d7dd587e5320dd
Andrew Sonnentag <ajsonnentag98@gmail.com>
2021-04-22 21:30:30 -0400

    fix build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo off
 if exist bfbb_stat_tracker.exe del bfbb_stat_tracker.exe
+if not exist build mkdir build
 pushd build
 ::cl ..\source\bfbb_stat_tracker.c /DDOLPHIN /MT /Od /Oi /Zi /link /SUBSYSTEM:WINDOWS /incremental:no /opt:ref user32.lib gdiplus.lib shlwapi.lib d3d9.lib
 cl ..\source\bfbb_stat_tracker.c /DDOLPHIN /MT /Od /Oi /Zi /link /incremental:no /opt:ref user32.lib gdiplus.lib shlwapi.lib d3d9.lib
